278b7f0cf854f1d0c0cf8ed5b11cf700d6db55d6
kelly r maass <kmaass77627@gmail.com>
2020-01-11 18:47:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,15 +27,21 @@ def self.drop_table
 end 
 
 def save
-  sql = <<-SQL   
-  INSERT INTO students (name, grade)
-  VALUES (?, ?)
-  SQL 
+  sql = <<-SQL INSERT INTO students (name, grade) VALUES (?, ?) SQL 
   
-  DB [:conn].execute(sql, self.name, self.grade)
+  DB[:conn].execute(sql, self.name, self.grade)
   
   @id = DB[:conn].execute("SELECT last_insert_row_id() FROM students")  [0] [0]
   end 
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+        VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
   
   def self.create(name, grade)
     student = Student.new(name, album)
